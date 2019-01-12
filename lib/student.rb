45ed52b0bf4b9488154a955a2deef10d52cd42ea
Rachel Grier <rgrier104@gmail.com>
2019-01-12 22:36:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,6 +57,18 @@ class Student
     end
   end
 
+  def self.all_students_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
