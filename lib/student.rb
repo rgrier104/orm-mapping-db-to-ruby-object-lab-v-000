710041bab6ee7cecda33ec297e06de0daf40bde9
Rachel Grier <rgrier104@gmail.com>
2019-01-12 22:40:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,6 +70,19 @@ class Student
     end.first
   end
 
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql,num).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.all_students_in_grade_X(x)
     sql = <<-SQL
       SELECT *
